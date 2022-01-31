e755a53d982e746e0bdf1ebaea1ac1552762b125
Konstvid <63913715+Konstvid@users.noreply.github.com>
2022-01-31 19:54:29 +0300

    H-4 Добавление ViewController's (#4)
    Co-authored-by: andrey mamleev <konstvid@mail.ru>

---
+++ Helper/Profile/ProfileViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileViewController.swift
+//  Helper
+//
+//  Created by Andrey on 30.01.2022.
+//
+
+import Foundation
+import UIKit
+
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        bind()
+    }
+    
+    private func configure() {
+        
+    }
+    
+    private func bind() {
+        
+    }
+}
---
+++ Helper/WeatherViewController.swift
@@ -0,0 +1,28 @@
+//
+//  WeatherViewController.swift
+//  Helper
+//
+//  Created by Andrey on 30.01.2022.
+//
+
+import Foundation
+import UIKit
+
+class WeatherVIewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        bind()
+    }
+    
+    private func configure() {
+        
+    }
+    
+    private func bind() {
+        
+    }
+}
+
+
