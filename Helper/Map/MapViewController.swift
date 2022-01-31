4609143f13ca6654c54d123743c98eaa39d3674e
andrey mamleev <konstvid@mail.ru>
2022-01-31 19:50:58 +0300

    H-4 Добавление ViewController's

---
+++ Helper/Map/MapViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MapViewController.swift
+//  Helper
+//
+//  Created by Andrey on 30.01.2022.
+//
+
+import Foundation
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        bind()
+    }
+    
+    private func configure() {
+        
+    }
+    
+    private func bind() {
+        
+    }
+    
+}
